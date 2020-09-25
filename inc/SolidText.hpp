041abe46888061512ce6309d60a4e789305be66f
VictorJorgeFGA <victor.eng.unb@gmail.com>
2020-09-25 09:07:11 -0300

    #13 Change SolidText to match VisualComponent changes

--- inc/SolidText.hpp
+++ inc/SolidText.hpp
@@ -8,11 +8,14 @@
 class SolidText : public VisualComponent
 {
 public:
-    static SolidText * newSolidText(const std::string & text, const std::string & font_name, uint8_t font_size, SDL_Color color);
+    static SolidText * newSolidText(const std::string & text,
+                                    const std::string & font_name,
+                                    uint8_t font_size,
+                                    SDL_Color color);
 
 private:
-    SolidText();
-    ~SolidText();
+    SolidText(uint16_t width = 0, uint16_t height = 0, SDL_Texture * texture = nullptr);
+    virtual ~SolidText();
 };
 
 #endif
