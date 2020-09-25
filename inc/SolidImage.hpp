9a32f865bbcd0279ea5b71a946175a8b561ea70e
VictorJorgeFGA <victor.eng.unb@gmail.com>
2020-09-25 08:31:40 -0300

    #13 Change SolidImage class to match changes at VisualComponent class

--- inc/SolidImage.hpp
+++ inc/SolidImage.hpp
@@ -7,11 +7,11 @@
 class SolidImage : public VisualComponent
 {
 public:
-    static SolidImage * newSolidImage(const std::string & image_name, int32_t width, int32_t height);
+    static SolidImage * newSolidImage(const std::string & image_name, uint16_t width, uint16_t height);
 
 private:
-    SolidImage();
-    ~SolidImage();
+    SolidImage(uint16_t width = 0, uint16_t height = 0, SDL_Texture * texture = nullptr);
+    virtual ~SolidImage();
 };
 
 #endif
